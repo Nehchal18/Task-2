abd7740419caad3a35d14aa4a839cf55d554524f
NiharikaKapoor <niharika1973.be22@chitkara.edu.in>
2023-03-22 18:56:51 +0530

    Adding classes

--- Main.py
+++ Main.py
@@ -58,3 +58,77 @@ current_letter_bg_x = 110
 indicators = []
 
 game_result = ""
+
+class Letter:
+    def _init_(self, text, bg_position):
+        # Initializes all the variables, including text, color, position, size, etc.
+        self.bg_color = "white"
+        self.text_color = "black"
+        self.bg_position = bg_position
+        self.bg_x = bg_position[0]
+        self.bg_y = bg_position[1]
+        self.bg_rect = (self.bg_x, self.bg_y, LETTER_SIZE, LETTER_SIZE)
+        self.text = text
+        self.text_position = (self.bg_x+36, self.bg_position[1]+34)
+        self.text_surface = GUESSED_LETTER_FONT.render(self.text, True, self.text_color)
+        self.text_rect = self.text_surface.get_rect(center=self.text_position)
+
+    def draw(self):
+        # Puts the letter and text on the screen at the desired positions.
+        pygame.draw.rect(SCREEN, self.bg_color, self.bg_rect)
+        if self.bg_color == "white":
+            pygame.draw.rect(SCREEN, FILLED_OUTLINE, self.bg_rect, 3)
+        self.text_surface = GUESSED_LETTER_FONT.render(self.text, True, self.text_color)
+        SCREEN.blit(self.text_surface, self.text_rect)
+        pygame.display.update()
+
+    def delete(self):
+        # Fills the letter's spot with the default square, emptying it.
+        pygame.draw.rect(SCREEN, "white", self.bg_rect)
+        pygame.draw.rect(SCREEN, OUTLINE, self.bg_rect, 3)
+        pygame.display.update()
+
+class Indicator:
+    def _init_(self, x, y, letter):
+        # Initializes variables such as color, size, position, and letter.
+        self.x = x
+        self.y = y
+        self.text = letter
+        self.rect = (self.x, self.y, 57, 75)
+        self.bg_color = OUTLINE
+
+    def draw(self):
+        # Puts the indicator and its text on the screen at the desired position.
+        pygame.draw.rect(SCREEN, self.bg_color, self.rect)
+        self.text_surface = AVAILABLE_LETTER_FONT.render(self.text, True, "white")
+        self.text_rect = self.text_surface.get_rect(center=(self.x+27, self.y+30))
+        SCREEN.blit(self.text_surface, self.text_rect)
+        pygame.display.update()
+
+class Button():
+	def _init_(self, x, y, image, scale):
+		width = image.get_width()
+		height = image.get_height()
+		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+		self.clicked = False
+
+	def draw(self, surface):
+		action = False
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#check mouseover and clicked conditions
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		#draw button on screen
+		surface.blit(self.image, (self.rect.x, self.rect.y))
+
+		return action
